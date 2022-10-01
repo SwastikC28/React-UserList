152926232af1281b38c14bc8687a996cd5d321cd
Swastik Chaudhary <swastikkumar50@gmail.com>
2022-10-01 12:40:12 +0530

    React Portal Added

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,22 +1,25 @@
-import React from "react"
+import ReactDOM from 'react-dom'
+
 import { Button } from "react-bootstrap"
 import classes from './Modal.module.css'
 
 const Modal = (props) => {
-    return <div className={classes.modal} >
-        <div className={classes.backdrop} onClick={props.removeError}></div>
-        <div className={classes.content}>
-            <h1 className={classes.header}>
-                {props.error}
-            </h1>
-            <div className={classes['error-content']}>
-                <p>
-                    {props.message}
-                </p>
-                <Button onClick={props.removeError}>Close</Button>
-            </div>
-        </div>
-    </div >
+    return < >
+        {ReactDOM.createPortal(<div className={classes.backdrop} onClick={props.removeError}></div>, document.getElementById('backdrop'))}
+        {
+            ReactDOM.createPortal(<div className={classes.content}>
+                <h1 className={classes.header}>
+                    {props.error}
+                </h1>
+                <div className={classes['error-content']}>
+                    <p>
+                        {props.message}
+                    </p>
+                    <Button onClick={props.removeError}>Close</Button>
+                </div>
+            </div>, document.getElementById('overlay'))
+        }
+    </>
 }
 
 
